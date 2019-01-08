2ae166e281f4fcd67aef64942e85a00c29a0d7c2
Matt Zuzolo <mattzuzolo@outlook.com>
2019-01-08 15:49:53 -0500

    Added nodemon launch script in package.json. Created Express erver

---
+++ server.js
@@ -0,0 +1,22 @@
+//external libraries
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+//local files
+// const { mongoose } = require("./db/mongoose");
+// const routes = require("./routes/routes");
+
+const PORT = process.env.PORT || 4000;
+
+//configure middleware
+app.use(cors());
+app.use(bodyParser.json());
+// routes(app);
+
+app.listen(PORT, () => {
+  console.log(`Started on port ${PORT}`);
+})
+
+module.exports = { app };
