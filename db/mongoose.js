98c26ce101885d86ff180b12b66fff65c4a48a61
Matt Zuzolo <mattzuzolo@outlook.com>
2019-01-08 16:05:22 -0500

    Built User model

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 //configure mongoose to use native JS promises
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://localhost:27017/tictactoe")
-  .then(console.log("Mongoose is connected..."))
-  .catch(console.error("Mongoose failed to conenct..."));
+mongoose.connect("mongodb://localhost:27017/tictactoe",  { useNewUrlParser: true })
+  .then(console.log("Mongoose is connected to the database..."))
+  .catch(error => console.error("Something went with mongoose: ", error))
 
 module.exports = { mongoose };
