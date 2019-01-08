491877931cc615a8189045aeea90179d94f1a987
Matt Zuzolo <mattzuzolo@outlook.com>
2019-01-08 16:12:41 -0500

    Built index and create methods in user controller

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,2 +1,14 @@
 const { User } = require("../models/user");
 const { ObjectID } = require("mongodb");
+
+module.exports = {
+  index(request, response, next){
+    User.find({})
+      .then(users => response.send({ users }))
+      .catch(next);
+  },
+  create(request, response, next){
+    console.log("CREATE REQUEST:", request);
+    console.log("CREATE REQUEST BODY: ", request.body);
+  },
+}
