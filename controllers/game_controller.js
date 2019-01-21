ceb73360066d656776ebc9c5d350d8d89be30670
Matt Zuzolo <mattzuzolo@outlook.com>
2019-01-21 15:25:06 -0500

    POST /games to create game with user reference is working

--- controllers/game_controller.js
+++ controllers/game_controller.js
@@ -6,4 +6,20 @@ module.exports = {
       .then(games => response.send({ games }))
       .catch(next);
   },
+  create(request, response, next){
+    let body = { turnsPlayed, userWon, user } = request.body;
+    let game = new Game(body);
+    game.save()
+      .then(game => response.send(game))
+      .catch(event => response.status(400).send(event));
+  }
 }
+
+
+// turnsPlayed: Number,
+//   userWon: Boolean,
+//   user: [{
+//     type: Schema.Types.ObjectId,
+//     ref: "user",
+//     required: true
+//   }],
